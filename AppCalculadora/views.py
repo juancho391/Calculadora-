b1c5a502da6c876d29d6e3e6a0095a17c509deaa
Juan Bedoya <juanchischis2010@hotmail.com>
2024-09-06 23:03:58 -0500

    Suma

--- AppCalculadora/views.py
+++ AppCalculadora/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class Suma(APIView):
+    def post(self, request):
+        # Obtenemos la lista de numeros que llego en el json 
+        listaNumeros = request.data.get('listaNumeros')
+        
+        # Calculamos la suma de todos los numeros en la lista
+        resultado = sum(listaNumeros)
+        
+        # Devuelve la respuesta con el resultado de la suma
+        return Response({'resultado': resultado})
+    
+
